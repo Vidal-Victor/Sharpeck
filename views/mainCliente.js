cfc3b6d21db5d4134c12d459142b05d6d74af138
KimuraRafael <rafaelkimura57@gmail.com>
2021-11-22 17:07:14 -0300

    TESTE

--- views/mainCliente.js
+++ views/mainCliente.js
@@ -1,13 +1,102 @@
-import React from "react"
-import { ScrollView, StyleSheet, Text, TextInput, View} from "react"
-export default function MainCliente(){
+import React from 'react';
+import { ContainerH, HeaderH, Footer, LogoImg, Title, css } from '../assets/css/Css';
+import { StyleSheet,KeyboardAvoidingView, Text, View,Button, SafeAreaView } from 'react-native';
+import logo from '../assets/img/LogoH.png';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
+
+export default function mainCliente (props)
+{
+    console.log(props);
 return(
-    <>
-    <View><Text>Yare yare daze</Text></View>
-    </>
+
+<KeyboardAvoidingView  behavior={Platform.OS == "ios" ? "padding" : "height"} style = {[css.container, css.shkbg]}>
+
+    <ContainerH colors={['#83EEBA','#6bd1aa', '#837EBA']}>
+        <HeaderH>
+        <LogoImg source= {logo} style= {{marginTop: 100}} resizeMode ="contain"></LogoImg>
+        <Title>Seja bem vindo ao Sharpeck!</Title>
+            
+            
+        </HeaderH>
+
+        <Footer>
+                
+                <Text style={styles.labelText}> Ofertas fresquinhas te aguardam, entre e veja!</Text>
+                
+                <TouchableOpacity style={styles.buttonContainer} onPress={() => props.navigation.navigate('Logar')}>
+                    <Text style={styles.buttonText}>AAAAAAAAAAAAAAAA FUNCIONA</Text>
+                </TouchableOpacity>
+
+                <View style={styles.divisor}>
+                 <View style={styles.divisorLine}></View>
+                 <Text style={{marginHorizontal:'3%', fontWeight:'bold', color:'#fff'}}>OU</Text>
+                 <View style={styles.divisorLine}></View>
+                </View>
+
+                <Text style={styles.labelText}> Caso não tenha uma conta FUNCIONADO: </Text>
+
+                <TouchableOpacity style={styles.buttonContainer}  onPress={() => props.navigation.navigate('Registro')}>
+                  <Text style={styles.buttonText}> Cadastre-se</Text>
+                </TouchableOpacity>
+        
+        </Footer>
+    </ContainerH>
+</KeyboardAvoidingView>
+
+
+
 );
-
-
 }
+/*<Button title='faça seu login' color = '#83EEBA' onPress={() => props.navigation.navigate('Login')} />
+        <Button title='Cadastre-se'   color = '#83EEBA' onPress={() => props.navigation.navigate('CadastroPj')} />*/
+const styles = StyleSheet.create({
+
+
+    buttonContainer: {
+    marginTop:'15%',    
+    backgroundColor: "#009688",
+    borderRadius: 10,
+    alignSelf:"center",
+    width:'90%',
+    paddingVertical: 10,
+    paddingHorizontal: 12
+
+    },
+
+    buttonText:{    
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    },
+
+    divisor:{
+        marginTop:'10%',
+        flexDirection:'row',
+        width: '90%',
+        alignSelf:"center",
+        justifyContent: 'center',
+    },
+
+    divisorLine:{
+        width: '45%',
+        height: 2,
+        alignSelf: "center",
+        backgroundColor:'#009688',
+        borderRadius: 5,
+    },
+
+    labelText:{
+        marginTop:40,
+        marginBottom:-45,
+        marginLeft:10,
+
+        fontSize: 18,
+        color: "#fff",
+        fontWeight: "bold",
+        alignSelf: "center",
+    },
+
+ });
