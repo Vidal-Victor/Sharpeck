86b28fdda6f4865e384fd9ee62591f3c90eb3fae
Rafael Manfrim <rafael.manfrim@etec.sp.gov.br>
2021-11-22 15:54:27 -0300

    Removendo espaços const firebaseConfig

--- views/firebase.js
+++ views/firebase.js
@@ -5,21 +5,13 @@ import  firebase from 'firebase';
 import 'firebase/storage';
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyBFOeo0Dubt5Qf4ICkUeSIgDWLap-xvooc",
-
   authDomain: "sharpeckbd-fc5fd.firebaseapp.com",
-
   databaseURL: "https://sharpeckbd-fc5fd-default-rtdb.firebaseio.com",
-
   projectId: "sharpeckbd-fc5fd",
-
   storageBucket: "sharpeckbd-fc5fd.appspot.com",
-
   messagingSenderId: "887798460053",
-
   appId: "1:887798460053:web:6ce0005f7fcfd2d571359c"
-
 };
 
 const app = firebase.initializeApp(firebaseConfig);
